bd30126eaf89156271fa6ffe975c9af8fdc2afa7
Luis Rosales <conejoconsal@hotmail.com>
2022-07-13 21:09:00 -0500

    se agrega funcion para cargar favoritos, pero no se termina

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
 const API_KEY = "api_key=617c85df-e898-4837-b119-16ac6cebe0f8";
-const URL = `https://api.thecatapi.com/v1/images/search?${API_KEY}&limit=3`;
-const catButton = document.getElementById('catButton');
-const imgSrc1 = document.getElementById('img1');
-const imgSrc2 = document.getElementById('img2');
-const imgSrc3 = document.getElementById('img3');
+const REAL_URL = "https://api.thecatapi.com/v1/";
+const randomCatsURL = `${REAL_URL}images/search?${API_KEY}&limit=3`;
+const favoriteCatsUrl = `${REAL_URL}favourites?${API_KEY}`;
 
-catButton.addEventListener( 'click', loadImage);
+const loadRandomCatsButton = document.getElementById('catButton');
+loadRandomCatsButton.addEventListener( 'click', loadRandomCats);
 
-async function loadImage(){
+async function loadRandomCats(){
 
-        const res = await fetch(URL);
+        const res = await fetch(randomCatsURL);
         const data = await res.json();
 
-        imgSrc1.src = data[0].url;
-        imgSrc2.src = data[1].url;
-        imgSrc3.src = data[2].url;
+        const imgElem1 = document.getElementById('img1');
+        const imgElem2 = document.getElementById('img2');
+        const imgElem3 = document.getElementById('img3');
+
+        console.log("Random Cats");
+        console.log(data);
+
+        imgElem1.src = data[0].url;
+        imgElem2.src = data[1].url;
+        imgElem3.src = data[2].url;
        
 }
 
-loadImage();
+loadRandomCats();
+
+async function loadFavoriteCats(){
+
+        const res = await fetch(favoriteCatsUrl);
+        const data = await res.json();
+
+        console.log("Favorite Cats");
+        console.log(data);
+
+}
+
+
+loadFavoriteCats();
 
 
